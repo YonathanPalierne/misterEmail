e631cc6227575274cb786bf781a4b90cd96f2c9a
YonathanPalierne <yonathan.palierne@gmail.com>
2024-08-30 19:44:26 +0300

    gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-
-import { Home } from './pages/Home';
+import { Home } from "./pages/Home"
 export function App() {
-
-    return (
-        <section className='main-app'>
-            <header className="app-header">
-                <section className="container">
-                    <h1>Log111</h1>
-                </section>
-            </header>
-
-            <main className='container'>
-                <Home />
-            </main>
-
-            <footer>
-                <section className="container">
-                    robotRights 2023 &copy;
-                </section>
-            </footer>
+  return (
+    <section className='main-app'>
+      <header className='app-header'>
+        <section className='container'>
+          <h1>Log111</h1>
         </section>
+      </header>
 
+      <main className='container'>
+        <Home />
+      </main>
 
-    )
+      <footer>
+        <section className='container'>robotRights 2023 &copy;</section>
+      </footer>
+    </section>
+  )
 }
-
